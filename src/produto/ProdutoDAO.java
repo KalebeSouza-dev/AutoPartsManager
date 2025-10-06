a07cc2707916345f6f9e58bae602f9a1a147121c
KalebeSouza-dev <kalebesouza.cc@gmail.com>
2025-10-05 21:09:28 -0300

    Produzindo Produto

--- src/produto/ProdutoDAO.java
+++ src/produto/ProdutoDAO.java
@@ -1,5 +1,25 @@
 package produto;
 
 public class ProdutoDAO {
-
+	public void cadastrarProduto() {
+		
+	}
+	public void removerProduto() {
+		
+	}
+	public void listarProduto() {
+		
+	}
+	public void aumentarProduto() {
+		
+	}
+	public void diminuirProduto() {
+		
+	}
+	public void alterarPrecoCompra() {
+		
+	}
+	public void alterarPrecoVenda() {
+		
+	}
 }
